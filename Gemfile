a25cc3355c7369d705ec892c6bc3759b6ce1e59b
Rolen Le <rolentle@gmail.com>
2013-10-15 11:57:12 -0600

    Added Foundations

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-contrib', require: 'sinatra/reloader'
+gem 'sinatra-assetpack', require: 'sinatra/assetpack'
+gem 'zurb-foundation'
+gem 'compass'
 
--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,35 @@
 require 'idea_box'
 
 class IdeaBoxApp < Sinatra::Base
+  register Sinatra::AssetPack
   set :method_override, true
   set :root, 'lib/app'
 
   configure :development do
     register Sinatra::Reloader
+  end
+
+  assets do
+    serve '/javascripts', from: 'javascripts'
+
+    js :foundation, [
+      'javascripts/foundation/foundation.js',
+      'javascripts/foundation/foundation.*.js'
+    ]
+
+    js :application, [
+      '/javascripts/vendor/*.js',
+      '/javascripts/app.js'
+    ]
+
+    serve '/stylesheets', from:  'stylesheets'
+    css :application, [
+      '/stylesheets/normalize.css',
+      '/stylesheets/app.css'
+    ]
+
+    js_compression :jsmin
+    css_compression :sass
   end
 
   not_found do
